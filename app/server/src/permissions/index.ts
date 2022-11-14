281fdad20d521003938d902231cb411e47847c84
djchp <dawid.sliwka321@gmail.com>
2022-11-14 21:41:52 +0100

    .

--- app/server/src/permissions/index.ts
+++ app/server/src/permissions/index.ts
@@ -32,4 +32,4 @@ export const permissions = shield({
     // incrementPostViewCount: rules.isAuthenticatedUser,
     // togglePublishPost: rules.isPostOwner,
   },
-})
+}, {allowExternalErrors: true})
