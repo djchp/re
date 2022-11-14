b261f7608e803062cb4fc283bafe3f7f0097ccc0
djchp <dawid.sliwka321@gmail.com>
2022-11-14 20:20:32 +0100

    .

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,16 +1,32 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import { createContext } from './context'
+import {ApolloServerPluginDrainHttpServer} from 'apollo-server-core'
 import { schema } from './schema'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
 
-const server = new ApolloServer({
-  schema,
-  context: createContext,
-})
+const app = express()
+// app.use(cors({origin: ['https://react-site-tb37.onrender.com/']}))
+app.use(cors())
 
-server.listen().then(({ url }) =>
+const startServer = async () => {
+  const httpServer = http.createServer(app)
+  const server = new ApolloServer({
+    schema,
+    context: createContext,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({httpServer})
+    ]
+  })
+  await server.start()
+  server.applyMiddleware({app})
+  const port = Number(process.env.PORT ?? 8080);
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ host: '0.0.0.0', port }, resolve)
+  );
   console.log(
-    `\
-🚀 Server ready at: ${url}
-⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
-  ),
-)
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+}
+startServer()
