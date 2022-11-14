e2ee40a546d37f0c2e5097b2033c977ed6ffe095
djchp <dawid.sliwka321@gmail.com>
2022-11-14 21:02:05 +0100

    .

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,32 +1,40 @@
 import { ApolloServer } from 'apollo-server-express'
 import { createContext } from './context'
-import {ApolloServerPluginDrainHttpServer} from 'apollo-server-core'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import { schema } from './schema'
 import express from 'express'
 import http from 'http'
 import cors from 'cors'
 
 const app = express()
-app.use(cors({origin: 'https://react-site-tb37.onrender.com'}))
+// app.use(cors({origin: 'https://react-site-tb37.onrender.com'}))
 // app.use(cors({origin: 'http://localhost:3000'}))
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://react-site-tb37.onrender.com',
+      'http://react-site-tb37.onrender.com',
+    ],
+    credentials: true,
+  }),
+)
 
 const startServer = async () => {
   const httpServer = http.createServer(app)
   const server = new ApolloServer({
     schema,
     context: createContext,
-    plugins: [
-      ApolloServerPluginDrainHttpServer({httpServer})
-    ]
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   })
   await server.start()
-  server.applyMiddleware({app})
-  const port = Number(process.env.PORT ?? 8080);
+  server.applyMiddleware({ app })
+  const port = Number(process.env.PORT ?? 8080)
   await new Promise<void>((resolve) =>
-    httpServer.listen({ host: '0.0.0.0', port }, resolve)
-  );
+    httpServer.listen({ host: '0.0.0.0', port }, resolve),
+  )
   console.log(
-    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
-  );
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`,
+  )
 }
 startServer()
